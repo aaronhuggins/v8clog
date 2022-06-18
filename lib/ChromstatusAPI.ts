78ba14a950e2d9cc5b3adbaa64f77edaae673ac0
Aaron Huggins <aaron.huggins@gmail.com>
2022-06-17 20:22:06 -0500

    Add API class for retrieving Chrome status details

---
+++ lib/ChromstatusAPI.ts
@@ -0,0 +1,59 @@
+// deno-lint-ignore-file no-explicit-any
+export class ChromstatusAPI {
+  #base = "https://chromestatus.com/api"
+  #version = "v0"
+  #XSSI_PREFIX = ")]}'\n"
+
+  #url (path: string, params?: any) {
+    if (params) {
+      const paramStr = new URLSearchParams(params).toString()
+      return new URL(`${path}?${paramStr}`, `${this.#base}/${this.#version}/`).toString()
+    }
+
+    return new URL(path, `${this.#base}/${this.#version}`).toString()
+  }
+
+  #safeParse (text: string) {
+    return new Promise<any>((resolve, reject) => {
+      if (!text) resolve(null)
+      if (text.startsWith(this.#XSSI_PREFIX)) {
+        return resolve(JSON.parse(text.substring(this.#XSSI_PREFIX.length)))
+      }
+  
+      try {
+        resolve(JSON.parse(text))
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+
+  async #request (path: string, expectStatus: number, params?: any, method = "GET") {
+    const resp = await fetch(this.#url(path, params), { method })
+
+    if (resp.status === expectStatus) {
+      return await resp.text().then((text) => this.#safeParse(text))
+    }
+
+    console.log(resp.status, resp.statusText)
+
+    return null
+  }
+
+  async channels (params?: ChannelsParams) {
+    return await this.#request("channels", 200, params)
+  }
+
+  async features (params: FeaturesParams) {
+    return await this.#request("features", 200, params)
+  }
+}
+
+interface ChannelsParams {
+  start: number
+  end: number
+}
+
+interface FeaturesParams {
+  milestone: number
+}
