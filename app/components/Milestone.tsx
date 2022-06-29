7a25871439f59bea0a080c960f4956ae6e78ef1d
Aaron Huggins <aaron.huggins@gmail.com>
2022-06-29 14:50:07 -0500

    Refactor milestone component for use w/ rss

--- app/components/Milestone.tsx
+++ app/components/Milestone.tsx
@@ -9,29 +9,24 @@ import type { FeatureData } from "../../lib/FeatureData.ts";
 import type { MilestonePair } from "../../lib/V8Metadata.ts"
 import { h } from "../jsx.ts"
 
-interface MilestoneInput {
-  detail: MilestonePair['detail']
-  data: FeatureData
-}
-
-export function Milestone ({ detail, data }: MilestoneInput) {
+export function Milestone ({ detail, data, style = true }: MilestoneInput) {
   return (
     <div>
-      <div class="uk-card uk-card-body uk-card-default uk-background-secondary uk-light">
-        <div class="uk-card-header">
-          <h3 class=""><a href={`/clog/${detail.mstone}`}>V8 release v{ detail.mstone }</a></h3>
-          <p class="uk-text-meta uk-margin-remove-top">
+      <div class={style ? "uk-card uk-card-body uk-card-default uk-background-secondary uk-light" : ""}>
+        <div class={style ? "uk-card-header" : ""}>
+          <h3><a href={`/clog/${detail.mstone}`}>V8 release v{ detail.mstone }</a></h3>
+          <p class={style ? "uk-text-meta uk-margin-remove-top" : ""}>
             Stable date: <time datetime={detail.stable_date}>{ new Date(detail.stable_date).toDateString() }</time>
           </p>
         </div>
-        { data.hasFeatures ? null : (<div class="uk-card-body"><p>No new features in this version.</p></div>) }
-        <MilestoneBody category="Enabled by default" featureDetails={data.enabled} />
-        <MilestoneBody category="Deprecated" featureDetails={data.deprecated} />
-        <MilestoneBody category="Removed" featureDetails={data.removed} />
-        <MilestoneBody category="Browser Intervention" featureDetails={data.browserIntervention} />
-        <MilestoneBody category="Origin trial" featureDetails={data.originTrial} />
-        <MilestoneBody category="In developer trial behind flag" featureDetails={data.developerTrial} />
-        <div class="uk-card-footer">
+        { data.hasFeatures ? null : (<div class={style ? "uk-card-body" : ""}><p>No new features in this version.</p></div>) }
+        <MilestoneCategory category="Enabled by default" featureDetails={data.enabled} style={style} />
+        <MilestoneCategory category="Deprecated" featureDetails={data.deprecated} style={style} />
+        <MilestoneCategory category="Removed" featureDetails={data.removed} style={style} />
+        <MilestoneCategory category="Browser Intervention" featureDetails={data.browserIntervention} style={style} />
+        <MilestoneCategory category="Origin trial" featureDetails={data.originTrial} style={style} />
+        <MilestoneCategory category="In developer trial behind flag" featureDetails={data.developerTrial} style={style} />
+        <div class={style ? "uk-card-footer" : ""}>
           <p><a href={`/clog/${detail.mstone}`}>Permalink</a></p>
         </div>
       </div>
@@ -40,29 +35,41 @@ export function Milestone ({ detail, data }: MilestoneInput) {
   )
 }
 
-function MilestoneBody (props: { category: string; featureDetails: FeatureDetail[] }) {
-  const { category, featureDetails } = props
-
+function MilestoneCategory ({ category, featureDetails, style = true }: CategoryInput) {
   if (featureDetails.length === 0) return null
 
   return (
-    <div class="uk-card-body uk-background-secondary uk-light">
+    <div class={style ? "uk-card-body uk-background-secondary uk-light" : ""}>
       <h4>{ category } { featureDetails.length === 0 ? '(None)' : `(${featureDetails.length})`}</h4>
       {
         featureDetails.map(val => (
-          <div class="uk-section uk-padding-small uk-padding-remove-top">
+          <div class={style ? "uk-section uk-padding-small uk-padding-remove-top" : ""}>
             <h5>{ val.name }</h5>
-            <p class="uk-text-meta uk-margin-remove-top formatted">
+            <p class={style ? "uk-text-meta uk-margin-remove-top formatted" : ""}>
               Category: { val.category }{val.flag_name && val.flag_name.startsWith('--') ? `\nFlag name: ${val.flag_name}` : ''}
             </p>
-            <p class="uk-light formatted">{ val.summary }</p>
-            <details>
-              <summary>JSON data</summary>
-              <pre><code class="json">{JSON.stringify(val, null, 2)}</code></pre>
-            </details>
+            <p class={style ? "uk-light formatted" : ""}>{ val.summary }</p>
+            {
+              style
+                ? (
+                  <details>
+                    <summary>JSON data</summary>
+                    <pre><code class="json">{JSON.stringify(val, null, 2)}</code></pre>
+                  </details>
+                )
+                : null
+            }
           </div>
         ))
       }
     </div>
   )
 }
+
+interface MilestoneInput {
+  detail: MilestonePair['detail']
+  data: FeatureData
+  style?: boolean
+}
+
+interface CategoryInput { category: string; featureDetails: FeatureDetail[]; style?: boolean }
