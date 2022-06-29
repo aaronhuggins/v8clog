4faaf7928013f3dc86427bdc8fdac8d9f51b328c
Aaron Huggins <aaron.huggins@gmail.com>
2022-06-29 13:27:46 -0500

    Refactor app entry point and support clog entries

--- app.tsx
+++ app.tsx
@@ -1,3 +1,4 @@
+// deno-lint-ignore-file no-explicit-any
 /** @jsx h */
 /// <reference no-default-lib="true"/>
 /// <reference lib="dom" />
@@ -6,86 +7,83 @@
 
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 import { h, renderSSR, Helmet } from "./app/jsx.ts"
-import { Header } from "./app/components/Header.tsx"
 import { Home } from "./app/components/Home.tsx"
 import { RSS } from "./app/components/RSS.tsx"
 import { StaticFile } from "./app/StaticFile.ts"
+import { V8Metadata } from "./lib/V8Metadata.ts";
+import { App } from "./app/components/App.tsx";
+import { ClogEntry } from "./app/components/ClogEntry.tsx";
 
-const App = () => {
-  return (
-    <div>
-      <Helmet>
-        <html lang="en" amp />
+const pattern = new URLPattern({ pathname: "/clog/:version" })
 
-        <body class="root" />
-        <body class="main" id="id" />
-
-        <title>V8 Clog: The Blog-style Changelog for the JavaScript Engine</title>
-        <meta name="description" content="V8 Clog" />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:creator" content="@aaronhugginsdev" />
-        <meta name="twitter:image" content="https://v8clog.deno.dev/static/v8clog.png"></meta>
-        <meta property="og:url" content="https://v8clog.deno.dev" />
-        <meta property="og:title" content="V8 Clog" />
-        <meta property="og:description" content="The (Unofficial) Blog-style Changelog for the V8 JavaScript Engine." />
-        <meta property="og:image" content="https://v8clog.deno.dev/static/v8clog.png" />
-      </Helmet>
-
-      <Header />
-      <Home />
-    </div>
-  )
-}
-
-serve(function server (request) {
+serve(async function server (request) {
   const url = new URL(request.url)
-  const file = new StaticFile(request.url)
+  const file = new StaticFile(url)
+
+  if (url.pathname.startsWith("/clog")) {
+    const parsed = pattern.exec(request.url)
+    const metadata = new V8Metadata()
+    try {
+      const detail = await metadata.milestone(parsed?.pathname.groups.version ?? '')
+      const features = await metadata.features(parsed?.pathname.groups.version ?? '')
+  
+      return renderHTML(<App><ClogEntry detail={detail} features={features} /></App>)
+    } catch (_error) { /* Missing or broken entries should rediect home. */ }
+  }
 
   if (file.isStatic) return file.response()
 
   switch (file.path) {
     case "./rss.xml": {
-      const rss = '<?xml version="1.0" encoding="utf-8"?>\n' + renderSSR(<RSS />)
-      return new Response(rss, {
-        headers: {
-          "Content-Type": "application/xml",
-          "Cache-Control": "no-cache, no-store"
-        }
-      })
+      return renderRSS(<RSS />)
     }
-    case "./": {
-      const app = renderSSR(<App />)
-      const { body, head, footer, attributes } = Helmet.SSR(app)
-    
-      const html = `
-      <!DOCTYPE html>
-      <html ${attributes.html.toString()}>
-        <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.14.3/dist/css/uikit.min.css" />
-          <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.5.1/build/styles/a11y-dark.min.css">
-          <link rel="stylesheet" href="/static/extensions.css" />
-          <script src="https://cdn.jsdelivr.net/npm/uikit@3.14.3/dist/js/uikit.min.js"></script>
-          <script src="https://cdn.jsdelivr.net/npm/uikit@3.14.3/dist/js/uikit-icons.min.js"></script>
-          <script src="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.5.1/build/highlight.min.js"></script>
-          <script>hljs.highlightAll();</script>
-          ${head.join('\n')}
-        </head>
-        <body ${attributes.body.toString()}>
-          ${body}
-          ${footer.join('\n')}
-        </body>
-      </html>`
-    
-      return new Response(html, {
-        headers: {
-          "Content-Type": "text/html"
-        }
-      })
+    case "./": {    
+      return renderHTML(<App><Home /></App>)
     }
     default: {
       return Response.redirect(url.origin, 307)
     }
   }
 })
+
+function renderRSS (input: any) {
+  const rss = '<?xml version="1.0" encoding="utf-8"?>\n' + renderSSR(input)
+  return new Response(rss, {
+    headers: {
+      "Content-Type": "application/xml",
+      "Cache-Control": "no-cache, no-store"
+    }
+  })
+}
+
+function renderHTML (input: any) {
+  const app = renderSSR(input)
+  const { body, head, footer, attributes } = Helmet.SSR(app)
+
+  const html = `
+  <!DOCTYPE html>
+  <html ${attributes.html.toString()}>
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.14.3/dist/css/uikit.min.css" />
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.5.1/build/styles/a11y-dark.min.css">
+      <link rel="stylesheet" href="/static/extensions.css" />
+      <script src="https://cdn.jsdelivr.net/npm/uikit@3.14.3/dist/js/uikit.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/uikit@3.14.3/dist/js/uikit-icons.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.5.1/build/highlight.min.js"></script>
+      <script>hljs.highlightAll();</script>
+      ${head.join('\n')}
+    </head>
+    <body ${attributes.body.toString()}>
+      ${body}
+      ${footer.join('\n')}
+    </body>
+  </html>`
+
+  return new Response(html, {
+    headers: {
+      "Content-Type": "text/html"
+    }
+  })
+}
