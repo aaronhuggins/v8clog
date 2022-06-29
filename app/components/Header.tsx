8d8e2bafa33105739eaf4cd47c8d7a2fab06954b
Aaron Huggins <aaron.huggins@gmail.com>
2022-06-29 10:58:41 -0500

    Improve header coloring

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,8 +8,8 @@ import { h } from "../jsx.ts"
 
 export function Header () {
   return (
-    <header id="header">
-      <nav class="uk-navbar uk-navbar-container" uk-navbar>
+    <header id="header" class="uk-background-primary" uk-navbar>
+      <nav class="uk-navbar-center">
         <div class="uk-navbar-left">
           <a href="/" class="uk-link-reset" style={{
             background: "url(/static/v8-outline.svg) no-repeat 50%",
@@ -33,13 +33,16 @@ export function Header () {
         <div class="uk-navbar-right">
           <ul class="uk-navbar-nav">
             <li class="uk-active">
-              <a href="/">Home</a>
+              <a href="/" style={{
+                fontWeight: "bold",
+                color: "white"
+              }}>Home</a>
             </li>
             <li>
-              <a href="/clog">Clog</a>
+              <a href="/clog" style={{ color: "white" }}>Clog</a>
             </li>
             <li>
-              <a href="https://v8.dev/">v8.dev</a>
+              <a href="https://v8.dev/"  style={{ color: "white" }}>v8.dev</a>
             </li>
           </ul>
         </div>
