47fa95f24bad03462eb1343f7360e930f350dd48
Aaron Huggins <aaron.huggins@gmail.com>
2022-06-29 11:37:17 -0500

    Move inline styles into sheet

--- app.tsx
+++ app.tsx
@@ -57,6 +57,7 @@ serve(function server (request) {
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.14.3/dist/css/uikit.min.css" />
+          <link rel="stylesheet" href="/static/extensions.css" />
           <script src="https://cdn.jsdelivr.net/npm/uikit@3.14.3/dist/js/uikit.min.js"></script>
           <script src="https://cdn.jsdelivr.net/npm/uikit@3.14.3/dist/js/uikit-icons.min.js"></script>
           ${head.join('\n')}
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -11,38 +11,21 @@ export function Header () {
     <header id="header" class="uk-background-primary" uk-navbar>
       <nav class="uk-navbar-center">
         <div class="uk-navbar-left">
-          <a href="/" class="uk-link-reset" style={{
-            background: "url(/static/v8-outline.svg) no-repeat 50%",
-            backgroundSize: "80%",
-            height: "6.5em",
-            width: "6.5em",
-          }}>
-            <span style={{ display: "none" }}>V8</span>
-            <div style={{
-              textAlign: "center",
-              marginTop: "2.25em",
-              color: "white",
-              fontWeight: "bold",
-              fontSize: "1.5em",
-              textDecoration: "none",
-              textStroke: "1px gray",
-              "-webkit-text-stroke": "1px gray"
-            }}>CLOG</div>
+          <a href="/" class="uk-link-reset logo">
+            <span class="logo">V8</span>
+            <div class="logo">CLOG</div>
           </a>
         </div>
         <div class="uk-navbar-right">
           <ul class="uk-navbar-nav">
             <li class="uk-active">
-              <a href="/" style={{
-                fontWeight: "bold",
-                color: "white"
-              }}>Home</a>
+              <a href="/">Home</a>
             </li>
             <li>
-              <a href="/clog" style={{ color: "white" }}>Clog</a>
+              <a href="/clog">Clog</a>
             </li>
             <li>
-              <a href="https://v8.dev/"  style={{ color: "white" }}>v8.dev</a>
+              <a href="https://v8.dev/">v8.dev</a>
             </li>
           </ul>
         </div>
