f6dd5de29d3e729c172b0dd1327ac59637b06164
Aaron Huggins <aaron.huggins@gmail.com>
2022-06-30 14:27:32 -0500

    Add favicon support

--- frontend/components/App.tsx
+++ frontend/components/App.tsx
@@ -18,6 +18,7 @@ export function App ({ active, children }: { active: ActiveNav; children?: any }
         <body class="main uk-background-secondary" id="main" />
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type="image/png" href="/static/v8clog.png"></link>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.14.3/dist/css/uikit.min.css" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.5.1/build/styles/a11y-dark.min.css" />
         <link rel="stylesheet" href="/static/extensions.css" />
