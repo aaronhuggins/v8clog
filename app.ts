7bdca6822f8c2188991eb221e9c14910a3f59425
Aaron Huggins <aaron.huggins@gmail.com>
2022-06-30 10:22:54 -0500

    Rename entrypoint file

--- app.ts
+++ app.ts
@@ -1,9 +1,3 @@
-/** @jsx h */
-/// <reference no-default-lib="true"/>
-/// <reference lib="dom" />
-/// <reference lib="dom.asynciterable" />
-/// <reference lib="deno.ns" />
-
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 import { Router } from "./frontend/Router.tsx"
 
