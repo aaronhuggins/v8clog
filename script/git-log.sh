c7347ed4764a2288abf1745ad7d5742cddc8e369
Aaron Huggins <aaron.huggins@gmail.com>
2022-07-03 12:38:25 -0500

    Fix yaml formatter in POC script

--- script/git-log.sh
+++ script/git-log.sh
@@ -2,7 +2,7 @@
 
 # The following commands are the minimum required to pull down just the logs so that they can be parsed into YAML format for consumption
 REPO="https://chromium.googlesource.com/v8/v8.git"
-YAML='- author: %n    name: "%aN"%n    email: "%aE"%n    date: "%aI"%n  commiter:%n    name: "%cN"%n    email: "%cE"%n    date: "%cI"%n  subject: |%n%w(0,4,4)%s%w(0,0,0)%n  sanitized_subject_line: %f%n  body: |%n%w(0,4,4)%b%n'
+YAML='- author:%n    name: "%aN"%n    email: "%aE"%n    date: "%aI"%n  commiter:%n    name: "%cN"%n    email: "%cE"%n    date: "%cI"%n  subject: |-%n%w(0,4,4)%s%w(0,0,0)%n  sanitized_subject_line: %f%n  body: |%n%w(0,4,4)>%b%n'
 AUTHOR='^((?!(V8 Autoroll|v8-ci-autoroll-builder)).*)$'
 
 rev_range () {
