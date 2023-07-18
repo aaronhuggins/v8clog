0d52e64b276f92316e7cac71f45cc997d8d3f55c
Aaron Huggins <aaron.huggins@runbox.com>
2023-07-18 11:17:47 -0500

    Seed using getAllData for performance

--- script/seed.ts
+++ script/seed.ts
@@ -2,20 +2,6 @@ import { BACKEND_TYPE } from "../backend/constants.ts";
 import { V8ChangeLog } from "../backend/v8/V8ChangeLog.ts";
 
 const v8clog = new V8ChangeLog(BACKEND_TYPE);
-const latest = await v8clog.getLatest();
-await Promise.all(
-  (await v8clog.getRange(v8clog.earliest, latest.milestone + 3)).map(
-    async (release) => {
-      const [features, changes] = await Promise.all([
-        release.getFeatures(),
-        release.getChanges(),
-      ]);
-      return {
-        release,
-        features,
-        changes,
-      };
-    },
-  ),
-);
+await v8clog.getLatest();
+await v8clog.getAllData(v8clog.earliest, v8clog.latest + 3);
 await v8clog.commit();
