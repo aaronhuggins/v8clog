e3c85c7300cbe540a6699094da43edfd91f74f4c
Aaron Huggins <aaron.huggins@runbox.com>
2023-07-22 11:51:05 -0500

    Set CACHE_TYPE as a constant

--- app.ts
+++ app.ts
@@ -1,9 +1,8 @@
 import { WebApiCache } from "./backend/Cache.ts";
+import { CACHE_TYPE } from "./backend/constants.ts";
 import { Router } from "./frontend/Router.tsx";
 
 const router = new Router();
-const cache = Deno.env.get("CACHE_TYPE") === "deno_builtin"
-  ? await caches.open("app")
-  : new WebApiCache();
+const cache = CACHE_TYPE ? await caches.open("app") : new WebApiCache();
 
 Deno.serve(router.respond(cache));
--- backend/constants.ts
+++ backend/constants.ts
@@ -1,6 +1,8 @@
 export const IS_DENO_DEPLOY = Deno.env.get("DENO_DEPLOYMENT_ID") !== undefined;
 export const BACKEND_TYPE =
   Deno.env.get("BACKEND_TYPE") as "deno_kv" | "json" | undefined ?? "json";
+export const CACHE_TYPE =
+  Deno.env.get("CACHE_TYPE") as "deno_builtin" | "memory" ?? "memory";
 export const V8 = {
   CHANGES: "v8_changes",
   FEATURES: "v8_features",
