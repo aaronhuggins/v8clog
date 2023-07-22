6484c7d5fd78fcfb4885f9ebffa002f1fae7d0f7
Aaron Huggins <aaron.huggins@runbox.com>
2023-07-22 11:44:46 -0500

    Fix typo for variable assignment

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import { Router } from "./frontend/Router.tsx";
 
 const router = new Router();
 const cache = Deno.env.get("CACHE_TYPE") === "deno_builtin"
-  ? new WebApiCache()
-  : await caches.open("app");
+  ? await caches.open("app")
+  : new WebApiCache();
 
 Deno.serve(router.respond(cache));
